c5a0531e30afb3e2702c1099483df8079d9747bb
tabVersion <tabvision@bupt.icu>
2021-03-25 16:17:32 +0800

    fix: request cloud for edge addr

--- interface/test_interface_client.py
+++ interface/test_interface_client.py
@@ -8,9 +8,21 @@ import time
 
 from protos import edge_interface_pb2
 from protos import edge_interface_pb2_grpc
+from protos import edge_cloud_pb2
+from protos import edge_cloud_pb2_grpc
+
+
+def get_edge_address():
+    channel = grpc.insecure_channel('localhost:50000')
+    request = edge_cloud_pb2_grpc.EdgeRegisterStub(channel)
+    resp = request.DeviceQuery(
+        edge_cloud_pb2.DeviceQueryRequest()
+    )
+    return resp.device_addr
 
 
 class TestEdgeInterface(unittest.TestCase):
+
     def test_interface(self):
         cloud_server = subprocess.Popen("cd ../cloud && python cloud_server.py &",
                                         stdout=subprocess.PIPE,
@@ -26,7 +38,8 @@ class TestEdgeInterface(unittest.TestCase):
                                  shell=True,
                                  preexec_fn=os.setsid)
         time.sleep(1)
-        channel = grpc.insecure_channel('localhost:50050')
+        edge_addrs = get_edge_address()
+        channel = grpc.insecure_channel(edge_addrs[0])
         request = edge_interface_pb2_grpc.UploadImageStub(channel)
         resp = request.GetImage(
             edge_interface_pb2.GetImageRequest(image=[1, 2, 3])
